37edee75822f01a74165b6457fc48a61c6c95a5f
foolish-pine <matsuda.takuya41@gmail.com>
2024-11-01 10:23:00 +0900

    chore: add astro settings

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,22 @@
 // @ts-check
+import sitemap from "@astrojs/sitemap";
 import { defineConfig } from 'astro/config';
-
 import react from '@astrojs/react';
-
+import robotsTxt from "astro-robots-txt";
 import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://example.com",
+  base: "/",
+  prefetch: {
+    prefetchAll: true,
+  },
+  server: {
+    host: true,
+    open: true,
+  },
   integrations: [react(), tailwind({
     applyBaseStyles: false,
-  })]
+  }), sitemap(), robotsTxt()]
 });
