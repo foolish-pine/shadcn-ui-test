10292583f9c49df9768492771622ce80d6b8a426
foolish-pine <matsuda.takuya41@gmail.com>
2024-11-01 10:40:13 +0900

    chore: add stylelint

---
+++ stylelint.config.cjs
@@ -0,0 +1,22 @@
+module.exports = {
+  extends: [
+    "stylelint-config-standard",
+    "stylelint-config-recess-order",
+    "stylelint-config-html",
+  ],
+  rules: {
+    "at-rule-no-unknown": [true,{"ignoreAtRules":["tailwind"]}],
+    "custom-property-pattern": null,
+    "function-name-case": null,
+    "keyframes-name-pattern": null,
+    "selector-class-pattern": null,
+    "selector-id-pattern": null,
+    "selector-pseudo-class-no-unknown": [
+      true,
+      {
+        ignorePseudoClasses: ["global"],
+      },
+    ],
+  },
+  ignoreFiles: ["dist/**", "**/node_modules/**"],
+};
